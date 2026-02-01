ae5cde01d3d085eff8770f3acd58883d52c9254b
Antonio Luis <antoniofarres8888@gmail.com>
2026-02-01 14:15:05 -0500

    all needed for it to run on the interface with basics cruds

--- modules/school/models/models.py
+++ modules/school/models/models.py
@@ -1,17 +1,7 @@
-# from odoo import models, fields, api
+from odoo import models, fields
 
-
-# class school(models.Model):
-#     _name = 'school.school'
-#     _description = 'school.school'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+class Student(models.Model): 
+    _name = "school.student"
+    _description = "Tabla de Estudiantes"
+    name = fields.Char(string="Nombre", required=True) 
+    age = fields.Integer(string="Edad")
