8053f39efefacb32d847412b91a8f51da8147def
Antonio Luis <antoniofarres8888@gmail.com>
2026-02-03 11:16:28 -0500

    making module for unique phone email constrain, State: before run dynamic translation comand

---
+++ modules/res_partner_contrains/models/models.py
@@ -0,0 +1,29 @@
+from odoo import models, api, _
+from odoo.exceptions import ValidationError
+
+
+class ResPartnerConstrain(models.Model):
+    _inherit ="res.partner"
+    
+    @api.constrains('email','phone')
+    def check_unique_email_phone(self):
+        for record in self:
+            if record.email:
+                existing_email = self.search([('email', '=', record.email),
+                             ('id', '!=', record.id)
+                             ], limit= 1)
+            if existing_email :
+                raise ValidationError(
+                    _('a contact with the same email its been register already')
+                    )
+            
+            if record.phone:
+                existing_phone = self.search([('phone','=', record.phone),
+                                              ('id', '!=', record.id)],
+                                             limit=1)
+                if existing_phone:
+                    ValidationError(
+                        _('a contact with the same phone already exist')
+                    )
+            
+                
