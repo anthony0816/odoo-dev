c14f40cba3aa71e27781c0eff917425f02bc6bb2
Antonio Luis <antoniofarres8888@gmail.com>
2026-02-17 13:06:34 -0500

    limpiando el ooo 19 para proyectos

--- modules/school/models/models.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class Student(models.Model): 
-    _name = "school.student"
-    _description = "Tabla de Estudiantes"
-    name = fields.Char(string="Nombre", required=True) 
-    age = fields.Integer(string="Edad")
